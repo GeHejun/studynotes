b06afae5f4303efdb9c2b986b1db720be3ed2f45
wanghui <1515764144@qq.com>
2019-06-23 10:28:40 +0800

    spring

--- spring/src/main/java/com/ghj/BeanCreateProcess.java
+++ spring/src/main/java/com/ghj/BeanCreateProcess.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class BeanCreateProcess {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext();
+        AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext(BeanCreator.class);
         annotationConfigApplicationContext.getBean("red");
     }
 
