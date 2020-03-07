9c6dbcb83f406c4709975a8e30dc49105296a3a4
wanghui <1515764144@qq.com>
2020-03-07 16:44:09 +0800

    spring aop

--- foundation/src/main/java/com/ghj/Test.java
+++
@@ -1,8 +0,0 @@
-package com.ghj;
-
-import java.util.concurrent.locks.ReentrantLock;
-
-public class Test {
-    Thread thread = new Thread();
-    ReentrantLock reentrantLock = new ReentrantLock();
-}
