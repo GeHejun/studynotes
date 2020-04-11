bb074d980bddf7a3808bd52755405035ecf30571
wanghui <1515764144@qq.com>
2020-04-11 20:58:16 +0800

    算法

--- algorithm/src/main/java/sort/HeapSort.java
+++ algorithm/src/main/java/sort/HeapSort.java
@@ -55,8 +55,8 @@ public class HeapSort {
     public static void main(String[] args) {
         int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};
         heapSort(arr, 8);
-        for (int i = 0; i < arr.length; i++) {
-            System.out.println(arr[i]);
+        for (int value : arr) {
+            System.out.println(value);
         }
 
     }
