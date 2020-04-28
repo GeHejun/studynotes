bce28fd09070a9c6c21aa1ff885d829cab5c9bc6
gehj <gehj@ziroom.com>
2020-04-28 13:49:37 +0800

    sb

---
+++ jvm/src/main/java/ClassLoaderTest.java
@@ -0,0 +1,8 @@
+/**
+ * @author gehj
+ * @version 1.0
+ * @description TODO
+ * @date 2020/4/28 13:37
+ */
+public class ClassLoaderTest {
+}
---
+++ jvm/src/main/java/MyClassLoader.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.FileChannel;
+import java.nio.channels.WritableByteChannel;
+
+/**
+ * @author gehj
+ * @version 1.0
+ * @description TODO
+ * @date 2020/4/28 13:34
+ */
+public class MyClassLoader extends ClassLoader {
+
+    public static void main(String[] args) throws ClassNotFoundException {
+        System.out.println(new MyClassLoader().findClass("ClassLoaderTest"));
+    }
+    @Override
+    protected Class<?> findClass(String name) throws ClassNotFoundException {
+        Class clazz = null;
+        File file = new File("D:\\gehj\\projects\\studynotes\\jvm\\src\\main\\java\\ClassLoaderTest.java");
+        try {
+            final byte[] classFileBytes = getClassFileBytes(file);
+            clazz = defineClass(name, classFileBytes, 0, classFileBytes.length);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return clazz;
+    }
+
+    private byte[] getClassFileBytes(File file) throws IOException {
+        FileInputStream fileInputStream = new FileInputStream(file);
+        FileChannel fileChannel = fileInputStream.getChannel();
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        WritableByteChannel writableByteChannel = Channels.newChannel(byteArrayOutputStream);
+        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(1024);
+        while (true) {
+            int read = fileChannel.read(byteBuffer);
+            if (read == 0 || read == -1) {
+                break;
+            }
+            byteBuffer.flip();
+            writableByteChannel.write(byteBuffer);
+            byteBuffer.clear();
+        }
+        fileInputStream.close();
+        return byteArrayOutputStream.toByteArray();
+    }
+}
