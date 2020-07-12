cf403c4304bb957668d0c12378867bb7d3e6cf6a
wanghui <1515764144@qq.com>
2020-07-12 22:28:30 +0800

    数组中重复数字

---
+++ algorithm/src/main/java/list/RepeaterNumber.java
@@ -0,0 +1,20 @@
+package list;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class RepeaterNumber {
+    public int findRepeatNumber(int[] nums) {
+        int[] tmp = new int[nums.length];
+        int i = 0;
+        while (i < nums.length) {
+            tmp[nums[i]]++;
+            if (tmp[nums[i]] > 1) {
+                return nums[i];
+            }
+            i++;
+        }
+        throw new IllegalArgumentException("数组中不存在重复数");
+    }
+}
