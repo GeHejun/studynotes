7be8aed082c39f0cba5a0b629842e223df5ff9fd
gehj <gehj@ziroom.com>
2020-07-12 17:54:06 +0800

    矩阵比较大小

--- algorithm/src/main/java/NumberIn2DArray.java
+++ algorithm/src/main/java/NumberIn2DArray.java
@@ -2,17 +2,23 @@ public class NumberIn2DArray {
 
     public static void main(String[] args) {
         int[][] matrix = {
-                {1,   4,  7, 11, 15},
-                {2,   5,  8, 12, 19},
-                {3,   6,  9, 16, 22},
-                {10, 13, 14, 17, 24},
-                {18, 21, 23, 26, 30}
+                {-5}
         };
-        System.out.println(findNumberIn2DArray(matrix, 20));
+        System.out.println(findNumberIn2DArray(matrix, -5));
     }
 
-    public static boolean findNumberIn2DArray(int[][] matrix, int target) {
+    private static boolean findNumberIn2DArray(int[][] matrix, int target) {
 
+        for (int i = matrix.length - 1; i >= 0; i-- ) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] > target) {
+                    break;
+                }
+                if (matrix[i][j] == target) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 }
