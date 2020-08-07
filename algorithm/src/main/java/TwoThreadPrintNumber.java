7fed247fab4c75b8831a5f4e110b732f4fbf9f84
wanghui <1515764144@qq.com>
2020-08-08 00:01:42 +0800

    数组中重复数字

---
+++ algorithm/src/main/java/TwoThreadPrintNumber.java
@@ -0,0 +1,49 @@
+public class TwoThreadPrintNumber {
+
+    public static volatile int flag = 0;
+    public static volatile int i = 0;
+    public static Object lock = new Object();
+
+    public static void main(String[] args) {
+
+        new Thread(() -> {
+            synchronized (lock) {
+                while (i < 100) {
+                    if (flag != 1) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println(i);
+                    i++;
+                    flag = 0;
+                    lock.notify();
+                }
+
+            }
+        }).start();
+
+        new Thread(() -> {
+            synchronized (lock) {
+                while (i < 100) {
+                    if (flag != 0 ) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    System.out.println(i);
+                    i++;
+                    flag = 1;
+                    lock.notify();
+                }
+
+            }
+
+
+        }).start();
+    }
+}
