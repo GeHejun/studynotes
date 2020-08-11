89b509a96e933d51cc185f627a21e69e60066c72
gehj <gehj@ziroom.com>
2020-08-11 14:35:55 +0800

    数组中重复数字

--- algorithm/src/main/java/ExchangeMoney.java
+++ algorithm/src/main/java/ExchangeMoney.java
@@ -1,6 +1,20 @@
 public class ExchangeMoney {
-    public static void exchangeMoney(int[] arr, int num) {
+    public static int exchangeMoney1(int[] arr, int target) {
+        if (arr == null || arr.length == 0 || target < 0) {
+            return 0;
+        }
+        return process1(arr, 0, target);
+    }
 
-
+    private static int process1(int[] arr, int index, int target) {
+        int result = 0;
+        if (index == arr.length) {
+            return target == result ? 1 : 0;
+        } else {
+            for (int i = 0; arr[index] * i <= target; i++) {
+                result += process1(arr, index + 1, target);
+            }
+        }
+        return result;
     }
 }
